7bec065089fbf698203d1ef3dfc6cd66b22e63bd
tqbrandao <tqbrandao@gmail.com>
2023-10-28 14:15:04 -0300

    set up tailwind css

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import logo from "../assets/logo/logo-header-final-centro.png";
+
+function Header() {
+  return (
+    <header>
+      <a href="#">
+        <img src={logo}></img>
+      </a>
+    </header>
+  );
+}
+
+export default Header;
